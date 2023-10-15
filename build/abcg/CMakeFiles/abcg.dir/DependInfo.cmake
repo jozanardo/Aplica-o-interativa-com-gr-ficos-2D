2bb50f451d9433feff183051cdb29df0b87848c6
Joao Zanardo <augustozanardo@yahoo.com.br>
2023-10-15 15:36:20 -0300

    inital commit

---
+++ build/abcg/CMakeFiles/abcg.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/joao/abcg/abcg/abcgApplication.cpp" "abcg/CMakeFiles/abcg.dir/abcgApplication.cpp.o" "gcc" "abcg/CMakeFiles/abcg.dir/abcgApplication.cpp.o.d"
+  "/home/joao/abcg/abcg/abcgException.cpp" "abcg/CMakeFiles/abcg.dir/abcgException.cpp.o" "gcc" "abcg/CMakeFiles/abcg.dir/abcgException.cpp.o.d"
+  "/home/joao/abcg/abcg/abcgImage.cpp" "abcg/CMakeFiles/abcg.dir/abcgImage.cpp.o" "gcc" "abcg/CMakeFiles/abcg.dir/abcgImage.cpp.o.d"
+  "/home/joao/abcg/abcg/abcgOpenGLError.cpp" "abcg/CMakeFiles/abcg.dir/abcgOpenGLError.cpp.o" "gcc" "abcg/CMakeFiles/abcg.dir/abcgOpenGLError.cpp.o.d"
+  "/home/joao/abcg/abcg/abcgOpenGLFunction.cpp" "abcg/CMakeFiles/abcg.dir/abcgOpenGLFunction.cpp.o" "gcc" "abcg/CMakeFiles/abcg.dir/abcgOpenGLFunction.cpp.o.d"
+  "/home/joao/abcg/abcg/abcgOpenGLImage.cpp" "abcg/CMakeFiles/abcg.dir/abcgOpenGLImage.cpp.o" "gcc" "abcg/CMakeFiles/abcg.dir/abcgOpenGLImage.cpp.o.d"
+  "/home/joao/abcg/abcg/abcgOpenGLShader.cpp" "abcg/CMakeFiles/abcg.dir/abcgOpenGLShader.cpp.o" "gcc" "abcg/CMakeFiles/abcg.dir/abcgOpenGLShader.cpp.o.d"
+  "/home/joao/abcg/abcg/abcgOpenGLWindow.cpp" "abcg/CMakeFiles/abcg.dir/abcgOpenGLWindow.cpp.o" "gcc" "abcg/CMakeFiles/abcg.dir/abcgOpenGLWindow.cpp.o.d"
+  "/home/joao/abcg/abcg/abcgTimer.cpp" "abcg/CMakeFiles/abcg.dir/abcgTimer.cpp.o" "gcc" "abcg/CMakeFiles/abcg.dir/abcgTimer.cpp.o.d"
+  "/home/joao/abcg/abcg/abcgTrackball.cpp" "abcg/CMakeFiles/abcg.dir/abcgTrackball.cpp.o" "gcc" "abcg/CMakeFiles/abcg.dir/abcgTrackball.cpp.o.d"
+  "/home/joao/abcg/abcg/abcgUtil.cpp" "abcg/CMakeFiles/abcg.dir/abcgUtil.cpp.o" "gcc" "abcg/CMakeFiles/abcg.dir/abcgUtil.cpp.o.d"
+  "/home/joao/abcg/abcg/abcgWindow.cpp" "abcg/CMakeFiles/abcg.dir/abcgWindow.cpp.o" "gcc" "abcg/CMakeFiles/abcg.dir/abcgWindow.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/joao/abcg/build/abcg/external/fmt/CMakeFiles/fmt.dir/DependInfo.cmake"
+  "/home/joao/abcg/build/abcg/external/imgui/CMakeFiles/imgui.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/examples/helloworld/opengl/CMakeFiles/helloworld.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/joao/abcg/examples/helloworld/opengl/main.cpp" "examples/helloworld/opengl/CMakeFiles/helloworld.dir/main.cpp.o" "gcc" "examples/helloworld/opengl/CMakeFiles/helloworld.dir/main.cpp.o.d"
+  "/home/joao/abcg/examples/helloworld/opengl/window.cpp" "examples/helloworld/opengl/CMakeFiles/helloworld.dir/window.cpp.o" "gcc" "examples/helloworld/opengl/CMakeFiles/helloworld.dir/window.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/joao/abcg/build/abcg/CMakeFiles/abcg.dir/DependInfo.cmake"
+  "/home/joao/abcg/build/abcg/external/fmt/CMakeFiles/fmt.dir/DependInfo.cmake"
+  "/home/joao/abcg/build/abcg/external/imgui/CMakeFiles/imgui.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
